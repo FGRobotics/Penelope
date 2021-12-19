dbd89a01ffa0e4795e4ce8a111c35f055827a7a3
Force Green Robotics 10265 <ForceGreenRobotics@gmail.com>
2021-12-19 15:28:18 -0600

    Update redDuckvison.java

--- cv ftc/redDuckvison.java
+++ cv ftc/redDuckvison.java
@@ -17,6 +17,15 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import java.util.Arrays;
 import java.util.Vector;
 
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+import java.io.IOException;
+
 /*
  * This is an example of a more complex path to really test the tuning.
  */
@@ -27,6 +36,80 @@ public class redDuck extends LinearOpMode {
     private ElapsedTime runtime = new ElapsedTime(0);
 
     public void runOpMode() throws InterruptedException {
+        
+        OpenCvWebcam webcam;
+        int test = 1;
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam"), cameraMonitorViewId);
+        //OpenCV Pipeline
+
+        ConceptCV myPipeline = new ConceptCV();
+        webcam.setPipeline(myPipeline);
+
+        OpenCvWebcam finalWebcam = webcam;
+        finalWebcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                finalWebcam.startStreaming(1920 ,1080, OpenCvCameraRotation.UPRIGHT);
+                telemetry.addData("Initialization passed ", test);
+                telemetry.update();
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("Init Failed ", errorCode);
+                telemetry.update();
+
+            }
+            
+            int location = 0;
+
+            location = ConceptCV.findTSE(ConceptCV.returnInput(),236,312,778,954,1416,1571,831,881);
+            //location of 0 = left, 1 = middle, 2 is right
+
+
+
+            telemetry.addData("Location: ",location);
+            telemetry.update();
+            
+
+
+            finalWebcam.stopStreaming(); 
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         SlidesAngle = hardwareMap.get(DcMotorEx.class, "SlidesAngle");
         LSlides = hardwareMap.get(DcMotorEx.class, "LSlides");
         Bin = hardwareMap.get(Servo.class, "Bin");
