1749dd804def65e544119f446cb2af19e243ad41
Force Green Robotics 10265 <ForceGreenRobotics@gmail.com>
2022-02-27 20:52:10 -0600

    Add files via upload

--- freightFrenzyTeleOpv1.java
+++ freightFrenzyTeleOpv1.java
@@ -25,6 +25,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.LED;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
@@ -53,7 +54,7 @@ import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kStatic;
 import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
 @TeleOp(name="FrenzyTelev1")
 public class freightFrenzyTeleOpv1 extends LinearOpMode {
-    private DcMotorEx leftFront, leftRear, rightRear, rightFront, LSlides, Wheel, Intake;
+    private DcMotorEx leftFront, leftRear, rightRear, rightFront, LSlides, Wheel, Intake, LEDs;
     private Servo Bin, Claw, UpTurret, HorizontallyTurret, initTurret;
     private CRServo extendTurret;
 
@@ -66,6 +67,7 @@ public class freightFrenzyTeleOpv1 extends LinearOpMode {
         leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
         rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
         rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        LEDs = hardwareMap.get(DcMotorEx.class, "LEDs");
         //SlidesAngle = hardwareMap.get(DcMotorEx.class, "SlidesAngle");
         LSlides = hardwareMap.get(DcMotorEx.class, "LSlides");
         Bin = hardwareMap.get(Servo.class, "Bin");
@@ -81,9 +83,9 @@ public class freightFrenzyTeleOpv1 extends LinearOpMode {
 
 
 
-        motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront, LSlides, Wheel, Intake);
+        motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront, LSlides, Wheel, Intake, LEDs);
 
-
+        LEDs.setDirection(DcMotorSimple.Direction.REVERSE);
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         rightRear.setDirection(DcMotorSimple.Direction.REVERSE);
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -145,18 +147,22 @@ public class freightFrenzyTeleOpv1 extends LinearOpMode {
             //wheelPower = gamepad1.right_trigger * -1;
             //Wheel motion at different speeds
             while(gamepad1.left_trigger > 0.0) {
-                Wheel.setPower(-0.2);
+                Wheel.setPower(-0.8);
+                //LEDs.setPower(Math.abs(Wheel.getPower()));
             }
             while(gamepad1.right_trigger > 0.0) {
                 Wheel.setPower(gamepad1.right_trigger * -1);
+                //LEDs.setPower(Math.abs(Wheel.getPower()));
             }
             Wheel.setPower(0);
 
             while (gamepad1.dpad_left) {
-                Wheel.setPower(0.2);
+                Wheel.setPower(0.8);
+                LEDs.setPower(Math.abs(Wheel.getPower()));
             }
             while (gamepad1.dpad_right) {
                 Wheel.setPower(0.8);
+                LEDs.setPower(Math.abs(Wheel.getPower()));
             }
             Wheel.setPower(0);
 
@@ -164,11 +170,14 @@ public class freightFrenzyTeleOpv1 extends LinearOpMode {
             if (gamepad2.right_bumper) {
 
                 Bin.setPosition(upRange);
+                LEDs.setPower(upRange);
 
             } else if (gamepad2.left_bumper) {
                 Bin.setPosition(downRange);
+                LEDs.setPower(downRange);
             } else if (gamepad2.dpad_left) {
                 Bin.setPosition(0.4);
+
             }
             //claw movement
             /*
@@ -184,25 +193,31 @@ public class freightFrenzyTeleOpv1 extends LinearOpMode {
                 leftRear.setPower(1);
                 rightFront.setPower(1);
                 rightRear.setPower(1);
+                LEDs.setPower(1);
             } else if (gamepad1.dpad_up) {
                 leftFront.setPower(-1);
                 leftRear.setPower(-1);
                 rightFront.setPower(-1);
                 rightRear.setPower(-1);
+                LEDs.setPower(1);
             } else {
                 leftFront.setPower(0);
                 leftRear.setPower(0);
                 rightFront.setPower(0);
                 rightRear.setPower(0);
+                LEDs.setPower(0);
             }
             //Intake
             if (gamepad2.right_trigger > 0.0) {
                 Intake.setPower(gamepad2.right_trigger);
+                LEDs.setPower(1);
                 Bin.setPosition(0.48);
             } else if (gamepad2.left_trigger > 0.0) {
                 Intake.setPower(gamepad2.left_trigger * -1);
+                LEDs.setPower(1);
             } else {
                 Intake.setPower(0);
+                LEDs.setPower(0);
                 //Bin.setPosition(0.49);
             }
             //Motor power setup
@@ -213,6 +228,7 @@ public class freightFrenzyTeleOpv1 extends LinearOpMode {
 
             //SlidesAngle.setPower(slidesAngPower);
             LSlides.setPower(LSlidesPower);
+            //LEDs.setPower(Math.abs(LSlidesPower));
             //Wheel.setPower(wheelPower);
 
             //Turret
@@ -311,6 +327,7 @@ public class freightFrenzyTeleOpv1 extends LinearOpMode {
         Wheel.setPower(0);
         LSlides.setPower(0);
         Intake.setPower(0);
+        LEDs.setPower(0);
     }
 
 
@@ -322,12 +339,14 @@ public class freightFrenzyTeleOpv1 extends LinearOpMode {
             leftFront.setPower(p);
             leftRear.setPower(p*-1);
             rightFront.setPower(p*-1);
+            LEDs.setPower(1);
         }
         else if(p < 0){
             leftFront.setPower(p);
             leftRear.setPower(p*-1);
             rightFront.setPower(p*-1);
             rightRear.setPower(p - p*0.8);
+            LEDs.setPower(1);
         }
 
 
