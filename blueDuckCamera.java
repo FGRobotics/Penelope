43c2b2b61a16e94e63236bc1f1086cd517a69b2e
Force Green Robotics 10265 <ForceGreenRobotics@gmail.com>
2022-03-05 08:44:34 -0600

    Add files via upload

--- blueDuckCamera.java
+++ blueDuckCamera.java
@@ -2,24 +2,30 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
 import org.openftc.easyopencv.OpenCvWebcam;
 
+
 import java.util.List;
+@Autonomous(group = "Drive")
 
 public class blueDuckCamera extends LinearOpMode {
-    private DcMotorEx LSlides, Wheel;
-
+    private DcMotorEx LSlides, Wheel, LEDs;
+    DistanceSensor distance, wheelSensor;
     private Servo Bin;
     public ElapsedTime wheelRun = new ElapsedTime(0);
 
@@ -29,24 +35,34 @@ public class blueDuckCamera extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
 
-        LSlides = hardwareMap.get(DcMotorEx.class, "LSlides");
-        Bin = hardwareMap.get(Servo.class, "Bin");
         Wheel = hardwareMap.get(DcMotorEx.class, "Wheel");
-
-//Bin start position - 0.4 is too low and cause problems coming back in, 0.5 cause issues intaking sometimes
+        LSlides = hardwareMap.get(DcMotorEx.class, "LSlides");
+        //LSlides.setDirection(DcMotorSimple.Direction.REVERSE);
+        Bin = hardwareMap.get(Servo.class, "Bin");
+        distance = hardwareMap.get(DistanceSensor.class, "toaster");
+        distance.getDistance(DistanceUnit.INCH);
+        LEDs = hardwareMap.get(DcMotorEx.class, "LEDs");
+        wheelSensor = hardwareMap.get(DistanceSensor.class, "wheelSensor");
+        wheelSensor.getDistance(DistanceUnit.INCH);
+        //Bin start position - 0.4 is too low and cause problems coming back in, 0.5 cause issues intaking sometimes
         Bin.setPosition(0.5);
+        LEDs.setDirection(DcMotorSimple.Direction.REVERSE);
+
 
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        Pose2d myPose = new Pose2d(10, 62, Math.toRadians(90));
+        Pose2d myPose = new Pose2d(-30, 62, Math.toRadians(90));
         drive.setPoseEstimate(myPose);
+        double difference = 0;
+        double turn = 0;
+        int location = 0;
 
         Trajectory fondue = drive.trajectoryBuilder(myPose)
                 //.back(20)
-                .lineToSplineHeading(new Pose2d(6, 51, Math.toRadians(50)))
+                .lineToSplineHeading(new Pose2d(-5, 37, Math.toRadians(89)))
                 .build();
-
-
-        waitForStart();
+        Trajectory duck = drive.trajectoryBuilder(fondue.end())
+                .lineToSplineHeading(new Pose2d(-65,57, Math.toRadians(80)))
+                .build();
         OpenCvWebcam webcam;
         int test = 1;
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
@@ -57,11 +73,13 @@ public class blueDuckCamera extends LinearOpMode {
         webcam.setPipeline(myPipeline);
 
         OpenCvWebcam finalWebcam = webcam;
+
         finalWebcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
             @Override
             public void onOpened() {
                 finalWebcam.startStreaming(1920, 1080, OpenCvCameraRotation.UPRIGHT);
                 telemetry.addData("Initialization passed ", test);
+
                 telemetry.update();
             }
 
@@ -72,118 +90,181 @@ public class blueDuckCamera extends LinearOpMode {
 
             }
         });
+        while(!opModeIsActive()){
 
-        sleep(4000);
+            //int testRun = 1;
+            //int finalTestRun = testRun;
+            //telemetry.addData("Pass number ", finalTestRun);
+            telemetry.update();
+            //red duck side 150,350,680,880,1380,1580,600,99
+            // red barrier side 440, 640, 1080, 1280, 1680, 1880, 650, 950
 
-        int location = 0;
+            ConceptCV.configureRects(150, 350, 680, 880, 1380, 1580, 600, 99);
+            ConceptCV.translateRects(0, 0, 0, 0);
+            location = ConceptCV.findTSE();
+            telemetry.addLine("location: " + location);
+            telemetry.update();
+            //testRun++;
+
+        }
 
 
-        //red duck side 150,350,680,880,1380,1580,600,99
-        // red barrier side 440, 640, 1080, 1280, 1680, 1880, 650, 950
-
-        ConceptCV.configureRects(150, 350, 680, 880, 1380, 1580, 600, 99);
-
-        location = ConceptCV.findTSE();
-        sleep(2000);
+        waitForStart();
+        if (isStopRequested()) return;
         location = ConceptCV.findTSE();
         telemetry.addLine("location: " + location);
         telemetry.update();
 
-
-        sleep(2000);
-
-
-        while (opModeIsActive()) {
-
-            finalWebcam.closeCameraDeviceAsync(new OpenCvCamera.AsyncCameraCloseListener() {
+        finalWebcam.closeCameraDeviceAsync(new OpenCvCamera.AsyncCameraCloseListener() {
 
 
-                @Override
-                public void onClose() {
-                    //finalWebcam.stopStreaming();
-                    telemetry.addLine("Closed");
-                    telemetry.update();
-                }
-            });
+            @Override
+            public void onClose() {
+                //finalWebcam.stopStreaming();
+                telemetry.addLine("Closed");
+                telemetry.update();
+            }
+        });
 
-            // Variable setup
-            int cPos = LSlides.getCurrentPosition();
-            double LSlidesPower = 0.0;
-            double LSlidesRotation = (LSlides.getCurrentPosition() / 1680.0);
-            double upRange = 0.5;
-            double downRange = 1.0;
-            //Telemetry
-            LSlidesRotation = LSlides.getCurrentPosition();
+        // Variable setup
+        int targetPos = 0;
+        int cPos = LSlides.getCurrentPosition();
+        double LSlidesPower = 0.0;
+        double LSlidesRotation = (LSlides.getCurrentPosition() / 1680.0);
+        double upRange = 0.5;
+        double downRange = 1.0;
+        //Telemetry
+        LSlidesRotation = LSlides.getCurrentPosition();
 
-            telemetry.addData("Slides current rotation: ", LSlidesRotation);
-            telemetry.addData("Slides tick position: ", LSlides.getCurrentPosition());
+        telemetry.addLine("location: " + location);
+        telemetry.update();
+
+        telemetry.addData("Slides current rotation: ", LSlidesRotation);
+        telemetry.addData("Slides tick position: ", LSlides.getCurrentPosition());
+        telemetry.update();
+
+        if (location == 0) {
+            LEDs.setPower(.5);
+            sleep(200);
+            LEDs.setPower(0);
+            drive.followTrajectory(fondue);
+            if (distance.getDistance(DistanceUnit.INCH) < 22) {
+                targetPos = 190 * (int) distance.getDistance(DistanceUnit.INCH) - 3850; //6650
+                //targetPos = 1700;
+            } else {
+                targetPos = 1700;
+            }
+
+        } else if (location == 1) {
+            LEDs.setPower(.5);
+            LEDs.setPower(0);
+            sleep(200);
+            LEDs.setPower(0.5);
+            sleep(200);
+            LEDs.setPower(0);
+            drive.followTrajectory(fondue);
+            if (distance.getDistance(DistanceUnit.INCH) < 22) {
+                targetPos = 190 * (int) distance.getDistance(DistanceUnit.INCH) -4210; //7700
+            } else {
+                targetPos = 2500;
+            }
+
+        } else if (location == 2) {
+            LEDs.setPower(.5);
+            LEDs.setPower(0);
+            sleep(200);
+            LEDs.setPower(0.5);
+            LEDs.setPower(0);
+            sleep(200);
+            LEDs.setPower(0.5);
+            sleep(200);
+            LEDs.setPower(0);
+            drive.followTrajectory(fondue);
+            if (distance.getDistance(DistanceUnit.INCH) < 22) {
+                targetPos = 190 * (int) distance.getDistance(DistanceUnit.INCH) - 4760;//9100
+            } else {
+                targetPos = 3600;
+            }
+
+        } else {
+            Wheel.setPower(0.5);
+            sleep(1000);
+            Wheel.setPower(0);
+        }
+
+            difference = 18 - distance.getDistance(DistanceUnit.INCH);
+            telemetry.addData("dISTANCE: ", distance.getDistance(DistanceUnit.INCH));
+            telemetry.addData("Differnec", difference);
             telemetry.update();
 
+            ElapsedTime extend = new ElapsedTime();
+            extend.reset();
+            while (extend.time() <= 2.0) {
+                if (!(LSlides.getCurrentPosition() >= targetPos - 100 && LSlides.getCurrentPosition() <= targetPos + 100)) {
+                    LSlides.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    LSlides.setPower(-0.8);
+                    LSlides.setTargetPosition(targetPos); //last number was 1600
 
-            if (location == 0) {
-                drive.followTrajectory(fondue);
+                    LSlides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    while (LSlides.isBusy()) {
+                        //telemetry.addData("Curent pos: ", LSlides.getCurrentPosition());
+                        //telemetry.update();
 
-                LSlides.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                LSlides.setPower(-0.8);
-                LSlides.setTargetPosition(1450);
-                LSlides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                while (LSlides.isBusy()) {
-                    telemetry.addData("Curent pos: ", LSlides.getCurrentPosition());
-                    telemetry.update();
+                        idle();
 
-                    idle();
-
+                    }
                 }
-            } else if (location == 1) {
-                drive.followTrajectory(fondue);
-                LSlides.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                LSlides.setPower(-0.8);
-                LSlides.setTargetPosition(3100);
-                LSlides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                while (LSlides.isBusy()) {
-                    telemetry.addData("Curent pos: ", LSlides.getCurrentPosition());
-                    telemetry.update();
-
-                    idle();
-
-                }
-
-            } else if (location == 2) {
-                LSlides.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-                LSlides.setPower(-0.8);
-                LSlides.setTargetPosition(3000);
-                LSlides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                while (LSlides.isBusy()) {
-                    telemetry.addData("Curent pos: ", LSlides.getCurrentPosition());
-                    telemetry.update();
-
-                    idle();
-
-                }
-            } else {
-                Wheel.setPower(0.5);
-                sleep(1000);
-                Wheel.setPower(0);
             }
-            sleep(1000);
-            Bin.setPosition(1.0);
-            sleep(1000);
-            Bin.setPosition(0.5);
-            sleep(1000);
-            LSlides.setPower(0.8);
-            LSlides.setTargetPosition((LSlides.getCurrentPosition() - 300) * -1);
-            LSlides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            while (LSlides.isBusy()) {
-                telemetry.addData("Curent pos: ", LSlides.getCurrentPosition());
-                telemetry.update();
 
+            if (LSlides.getCurrentPosition() >= targetPos - 100 && LSlides.getCurrentPosition() <= targetPos + 100) {
+                sleep(1000);
+                Bin.setPosition(1.0);
+                sleep(1000);
+                Bin.setPosition(0.5);
+                sleep(1000);
+            } else {
+                if (!(LSlides.getCurrentPosition() >= targetPos - 100 && LSlides.getCurrentPosition() <= targetPos + 100)) {
+                    LSlides.setPower(-0.8);
+                    LSlides.setTargetPosition(targetPos); //last number was 1600
+
+                    LSlides.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    while (LSlides.isBusy()) {
+                        //telemetry.addData("Curent pos: ", LSlides.getCurrentPosition());
+                        //telemetry.update();
+
+                        idle();
+
+                    }
+                }
+                if (LSlides.getCurrentPosition() >= targetPos - 100 && LSlides.getCurrentPosition() <= targetPos + 100) {
+                    sleep(1000);
+                    Bin.setPosition(1.0);
+                    sleep(1000);
+                    Bin.setPosition(0.5);
+                    sleep(1000);
+                }
+            }
+
+            LSlides.setTargetPosition(0);
+            LSlides.setPower(-0.8);
+            while (LSlides.isBusy()) {
                 idle();
             }
+            LSlides.setPower(0);
+            if (LSlides.getCurrentPosition() >= 0 && LSlides.getCurrentPosition() <= 100) {
+                //drive.followTrajectory(park);
+            } else {
+                LSlides.setTargetPosition(0);
+                LSlides.setPower(-0.8);
+                while (LSlides.isBusy()) {
+                    idle();
+                }
+                LSlides.setPower(0);
+                sleep(800);
 
-            //drive.followTrajectory(fondue);
+            }
 
         }
 
     }
 
-}
