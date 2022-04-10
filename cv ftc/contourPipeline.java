3322629dc6e637e34b44fb25f7a51cafdd3a950a
Force Green Robotics 10265 <ForceGreenRobotics@gmail.com>
2022-04-10 16:01:12 -0500

    Add files via upload

--- cv ftc/contourPipeline.java
+++ cv ftc/contourPipeline.java
@@ -19,75 +19,81 @@ public class contourPipeline extends OpenCvPipeline {
 
     @Override
     public Mat processFrame(Mat input) {
-        Mat end = input;
+        try {
+            Mat end = input;
 
-        Mat src = input;
-
-
-        Scalar scalarLowerYCrCb = new Scalar(  0.0, 0.0, 0.0);
-        Scalar scalarUpperYCrCb = new Scalar(255.0, 120.0, 200.0);
+            Mat src = input;
 
 
-        //Converts space to ycrcb
-        Imgproc.cvtColor(src, src, Imgproc.COLOR_RGB2YCrCb);
-        //filters out all colors not in this range
-        Core.inRange(src, scalarLowerYCrCb, scalarUpperYCrCb, src);
-        // Remove Noise
-        Imgproc.morphologyEx(src, src, Imgproc.MORPH_OPEN, new Mat());
-        Imgproc.morphologyEx(src, src, Imgproc.MORPH_CLOSE, new Mat());
-        // GaussianBlur
-        Imgproc.blur(src, src, new Size(20, 20));
+            //Scalar scalarLowerYCrCb = new Scalar(0.0, 0.0, 0.0);
+            //Scalar scalarUpperYCrCb = new Scalar(255.0, 100.0, 170.0);
 
 
+            Scalar scalarLowerYCrCb = new Scalar(30.0, 71.4, 40.0);//HSV for now
+            Scalar scalarUpperYCrCb = new Scalar(80.0, 255.0, 255.0);//HSV for now
 
-        //Finding Contours
+            //Converts space to ycrcb
+            Imgproc.cvtColor(src, src,Imgproc.COLOR_BGR2HSV);
+            //filters out all colors not in this range
+            Core.inRange(src, scalarLowerYCrCb, scalarUpperYCrCb, src);
+            // Remove Noise
+            Imgproc.morphologyEx(src, src, Imgproc.MORPH_OPEN, new Mat());
+            Imgproc.morphologyEx(src, src, Imgproc.MORPH_CLOSE, new Mat());
+            // GaussianBlur
+            Imgproc.blur(src, src, new Size(10, 10));
 
-        ArrayList<MatOfPoint> contours = new ArrayList<>();
 
-        Mat hierarchey = new Mat();
-        //finds largest contour
-        Imgproc.findContours(src, contours, hierarchey, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+            //Finding Contours
 
-        int largestIndex = 0;
-        int largest = contours.get(0).toArray().length;
+            ArrayList<MatOfPoint> contours = new ArrayList<>();
+
+            Mat hierarchey = new Mat();
+            //finds largest contour
+            Imgproc.findContours(src, contours, hierarchey, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+            int largestIndex = 0;
+            int largest = contours.get(0).toArray().length;
 
 
-        for(int i = 0; i<contours.size();i++) {
-            int currentSize = contours.get(i).toArray().length;
-            if(currentSize>largest) {
+            for (int i = 0; i < contours.size(); i++) {
+                int currentSize = contours.get(i).toArray().length;
+                if (currentSize > largest) {
 
-                largest = currentSize;
-                largestIndex = i;
+                    largest = currentSize;
+                    largestIndex = i;
+                }
+
             }
+
+
+            //Draw rectangle on largest contours
+
+            MatOfPoint2f areaPoints = new MatOfPoint2f(contours.get(largestIndex).toArray());
+
+            Rect rect = Imgproc.boundingRect(areaPoints);
+
+            Imgproc.rectangle(end, rect, new Scalar(255, 0, 0));
+            ;
+
+            midpoint = (rect.x + (rect.x + rect.width)) / 2;
+
+
+            int middleBound = 600;
+
+            int rightBound = 1300;
+
+
+            Imgproc.line(end, new Point(middleBound, 1920), new Point(middleBound, 0), new Scalar(255, 0, 0));
+
+            Imgproc.line(end, new Point(rightBound, 1920), new Point(rightBound, 0), new Scalar(255, 0, 0));
+
+            return end;
+        }catch(IndexOutOfBoundsException e){
+            int one = 1;
 
         }
 
-
-        //Draw rectangle on largest contours
-
-        MatOfPoint2f areaPoints = new MatOfPoint2f(contours.get(largestIndex).toArray());
-
-        Rect rect = Imgproc.boundingRect(areaPoints);
-
-        Imgproc.rectangle(end, rect, new Scalar(255,0,0));;
-
-        midpoint = (rect.x + (rect.x + rect.width)) /2;
-
-
-        int middleBound = 600;
-
-        int rightBound = 1300;
-
-
-
-
-
-
-        Imgproc.line(end, new Point(middleBound,1920), new Point(middleBound,0), new Scalar(255,0,0));
-
-        Imgproc.line(end, new Point(rightBound,1920), new Point(rightBound,0), new Scalar(255,0,0));
-
-        return end;
+        return input;
 
     }
 
